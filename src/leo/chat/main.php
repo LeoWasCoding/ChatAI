64dda570539b810e8a720e2eebe7007643dc255d
LeoWasCoding <98230552+LeoWasCoding@users.noreply.github.com>
2023-05-27 22:50:02 +0500

    Create main.php

---
+++ src/leo/chat/main.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace YourNamespace;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerChatEvent;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener {
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getLogger()->info("ChatBotPlugin enabled!");
+    }
+
+    public function onPlayerChat(PlayerChatEvent $event) {
+        $message = $event->getMessage();
+
+        // Convert the message to lowercase for case-insensitive comparison
+        $message = strtolower($message);
+
+        // Check if the message contains "hi"
+        if (strpos($message, 'hi') !== false) {
+            $player = $event->getPlayer();
+            $player->sendMessage("Hi there, ".$player->getName()."!");
+        }
+    }
+}
+
